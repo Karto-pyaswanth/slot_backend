c8471cf70db0f35a6797f63a3183508824fe4364
karto-pyaswanth <yaswanth.p@kartoffel.in>
2025-03-06 12:52:14 +0530

    slot management changes

--- src/routes/slotRoutes.js
+++ src/routes/slotRoutes.js
@@ -8,6 +8,6 @@ router.post('/add/slots', slotController.createSlot); // Create a new slot
 router.get('/slots', slotController.getSlots); // Get all slots
 router.get('/slots/:id', slotController.getSlotById); // Get a slot by ID
 router.delete('/slots/:id', slotController.deleteSlot); // Delete a slot by ID
-router.put('/slots/:id', slotController.updateSlot); // Update a slot by ID
+router.put('/slots/:id', slotController.updateSlot); // Update a slot by ID l
 
 module.exports = router;
